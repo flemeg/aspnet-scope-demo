e1e9007efdead9b903dcad810950d1719744ac5d
Flemeg Amorim <flemeg@gmail.com>
2021-05-21 09:51:35 -0300

    Add project files.

---
+++ DependencyInjection/Operation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceScopeDemo.DependencyInjection
+{
+    public class Operation : IOperationTransient, IOperationScoped, IOperationSingleton, IOperationSingletonInstance
+    {
+        Guid _guid;
+        public Operation() : this(Guid.NewGuid())
+        {
+
+        }
+
+        public Operation(Guid guid)
+        {
+            _guid = guid;
+        }
+
+        public Guid OperationId => _guid;
+    }
+}
